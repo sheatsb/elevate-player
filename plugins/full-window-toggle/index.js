aac19d68567241ad13b05edfb4da524d804ddb9e
b sheats <sheatsb@users.noreply.github.com>
2020-10-10 17:47:47 -0400

    add videojs-plus plugins

---
+++ plugins/full-window-toggle/index.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+/* VERSION: 1.6.2 */
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('video.js')) :
+  typeof define === 'function' && define.amd ? define(['video.js'], factory) :
+  (global = global || self, factory(global.videojs));
+}(this, (function (videojs) { 'use strict';
+
+  videojs = videojs && Object.prototype.hasOwnProperty.call(videojs, 'default') ? videojs['default'] : videojs;
+
+  function _inheritsLoose(subClass, superClass) {
+    subClass.prototype = Object.create(superClass.prototype);
+    subClass.prototype.constructor = subClass;
+    subClass.__proto__ = superClass;
+  }
+
+  var FullWindowToggle = /*#__PURE__*/function (_videojs$getComponent) {
+    _inheritsLoose(FullWindowToggle, _videojs$getComponent);
+
+    function FullWindowToggle(player, options) {
+      var _this;
+
+      _this = _videojs$getComponent.call(this, player, options) || this;
+      player.requestFullscreen = player.enterFullWindow.bind(player);
+      player.enterFullScreen = player.enterFullWindow.bind(player);
+      player.exitFullscreen = player.exitFullWindow.bind(player);
+
+      player.isFullscreen = function () {
+        return !!player.isFullWindow;
+      };
+
+      player.on(['enterFullWindow', 'exitFullWindow'], function () {
+        player.toggleFullscreenClass_();
+        player.trigger('fullscreenchange');
+      });
+      return _this;
+    }
+
+    var _proto = FullWindowToggle.prototype;
+
+    _proto.handleClick = function handleClick() {
+      if (!this.player_.isFullWindow) {
+        this.player_.enterFullScreen();
+      } else {
+        this.player_.exitFullscreen();
+      }
+    } // Since `FullWindowToggle` are extends from `FullscreenToggle`,
+    // disable function will be called if the browser does not support fullscreen API
+    // However, `FullWindowToggle` is assumed support in all platform. So the function should be override
+    ;
+
+    _proto.disable = function disable(force) {
+      force && _videojs$getComponent.prototype.disable.call(this);
+    };
+
+    return FullWindowToggle;
+  }(videojs.getComponent('FullscreenToggle'));
+
+  videojs.registerComponent('FullWindowToggle', FullWindowToggle);
+  var controlBarChildren = videojs.getComponent('ControlBar').prototype.options_.children;
+  var fullScreenButtonIndex = controlBarChildren.indexOf('FullscreenToggle');
+  controlBarChildren[fullScreenButtonIndex] = 'FullWindowToggle';
+
+})));
+//# sourceMappingURL=index.js.map
