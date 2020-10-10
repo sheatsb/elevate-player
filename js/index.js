083744f8fd90f1a467b6402f49b45b534f796c40
b sheats <sheatsb@users.noreply.github.com>
2020-10-10 17:34:56 -0400

    we didn't need no stinking packages

--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
-var videoJS = videojs("video");
-videoJS.src({
-	src:
-		"https://live-festival-2020-elevate-use2.streaming.media.azure.net/16be96a9-3741-495e-a302-bf2a499d1527/b663334064.ism/manifest(format=m3u8-aapl)",
-	type: "application/x-mpegURL",
-});
+var player = videojs("video");
